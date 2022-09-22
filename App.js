9a70a3a837ee0a422958aa5124e88dc305e2c0d2
unknown <renansilva.dev@gmail.com>
2022-09-22 11:56:47 -0300

    pronto

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins';
-import Detalhes from './screens/detalhes';
-import Home from './screens/home';
+
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import Detalhes from './screens/detalhes';
+import Home from './screens/home';
+
+const App = () => {
 
   const fontsLoaded = useFonts({
     Poppins_400Regular,
@@ -21,19 +22,12 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name={'home'} component={Home}/>
-        <Stack.Screen name={'detalhes'} component={Detalhes}/>
+        <Stack.Screen options={{title: 'Home'}} name={'home'} component={Home}/>
+        <Stack.Screen options={{title: 'Detalhes'}} name={'detalhes'} component={Detalhes}/>
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
--- screens/home.jsx
+++ screens/home.jsx
@@ -3,15 +3,18 @@ import { View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 import { ref, onValue } from 'firebase/database'
 import { db } from '../config/firebase'
 
-const Home = () => {
+const Home = ({navigation}) => {
 
   const [alunos, setAlunos] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   const getAlunos = async () => {
     const refAlunos = await ref(db, 'Alunos/')
     await onValue(refAlunos, (snapshot) => {
-      if (snapshot.exists()) setAlunos(snapshot.val())
-      console.log(snapshot.val())
+      if(snapshot.exists()) {
+        setAlunos(snapshot.val())
+        setIsLoading(false)
+      }
     })
   }
 
@@ -19,22 +22,43 @@ const Home = () => {
     getAlunos()
   }, [])
 
-  return (
-    <View>
+  if(!isLoading) return (
+    <View style={styles.content}>
+      <Text style={styles.title}>Alunos</Text>
       {
-        alunos.map(aluno => (
-          <TouchableOpacity onPress={navigation.navigate('detalhes', {aluno: aluno})}>
-            <Text>{aluno.Nome}</Text>
+        alunos.map((aluno, index) => (
+          <TouchableOpacity 
+            key={index}
+            style={styles.button}
+            onPress={() => navigation.navigate('detalhes', {aluno: aluno})}
+          >
+            <Text style={styles.text}>{aluno.Nome}</Text>
           </TouchableOpacity>
         ))
       }
     </View>
   )
+  else return <Text>Loading...</Text>
 }
 
 const styles = StyleSheet.create({
   content: {
-
+    padding: 10
+  },
+  button: {
+    width: '100%',
+    backgroundColor: 'rgba(0, 0, 0, .3)',
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderRadius: 5
+  },
+  title: {
+    fontFamily: 'Poppins_700Bold',
+    fontSize: 36
+  },
+  text: {
+    fontFamily: 'Poppins_400Regular'
   }
 })
 
